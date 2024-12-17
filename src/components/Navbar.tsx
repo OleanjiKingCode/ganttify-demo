5e15e2dfc4a610c9c5391b8cfeac05003a0f16df
OleanjiKingCode <holamilean56@gmail.com>
2024-12-17 16:50:28 +0300

    animations for navbar and buttons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,8 +4,46 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import { motion } from "motion/react";
 import Image from "next/image";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import { Ri24HoursFill } from "react-icons/ri";
+import { cn } from "@/lib/utils";
+import { IoMdStar } from "react-icons/io";
 
 export const Navbar = () => {
+  const NavMenu = (
+    <ul className="grid gap-3 p-4 md:w-[500px] lg:w-[600px] lg:grid-cols-[.75fr_1fr]">
+      <li className="row-span-3">
+        <NavigationMenuLink asChild>
+          <a
+            className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+            href="/"
+          >
+            <Ri24HoursFill className="h-6 w-6" />
+            <div className="mb-2 mt-4 text-lg font-medium">Oleanji</div>
+            <p className="text-sm leading-tight text-muted-foreground">
+              A console.log developer
+            </p>
+          </a>
+        </NavigationMenuLink>
+      </li>
+      <ListItem href="/docs" title="Introduction">
+        Re-usable components built using Radix UI and Tailwind CSS.
+      </ListItem>
+      <ListItem href="/docs/installation" title="Installation">
+        How to install dependencies and structure your app.
+      </ListItem>
+      <ListItem href="/docs/primitives/typography" title="Typography">
+        Styles for headings, paragraphs, lists...etc
+      </ListItem>
+    </ul>
+  );
   return (
     <motion.div
       initial={{ opacity: 0, y: "100px" }}
@@ -44,71 +82,115 @@ export const Navbar = () => {
           </motion.span>
         </div>
         <div className="flex gap-6 text-xs items-center">
-          <motion.span
-            initial={{ scale: 0.5, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }}
-            transition={{
-              duration: 0.4,
-              delay: 0.7,
-              ease: "easeIn",
-            }}
-          >
-            Product
-          </motion.span>
-          <motion.span
-            initial={{ scale: 0.5, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }}
-            transition={{
-              duration: 0.4,
-              delay: 0.8,
-              ease: "easeIn",
-            }}
-          >
-            Solutions
-          </motion.span>
-          <motion.span
-            initial={{ scale: 0.5, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }}
-            transition={{
-              duration: 0.4,
-              delay: 0.9,
-              ease: "easeIn",
-            }}
-          >
-            Resources
-          </motion.span>
-          <motion.span
-            initial={{ scale: 0.5, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }}
-            transition={{
-              duration: 0.4,
-              delay: 1,
-              ease: "easeIn",
-            }}
-          >
-            Company
-          </motion.span>
-          <motion.span
-            initial={{ scale: 0.5, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }}
-            transition={{
-              duration: 0.4,
-              delay: 1.1,
-              ease: "easeIn",
-            }}
-          >
-            Pricing
-          </motion.span>
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>
+                  <motion.span
+                    initial={{ scale: 0.5, opacity: 0 }}
+                    animate={{ scale: 1, opacity: 1 }}
+                    transition={{
+                      duration: 0.4,
+                      delay: 0.7,
+                      ease: "easeIn",
+                    }}
+                  >
+                    Product
+                  </motion.span>
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>{NavMenu}</NavigationMenuContent>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>
+                  <motion.span
+                    initial={{ scale: 0.5, opacity: 0 }}
+                    animate={{ scale: 1, opacity: 1 }}
+                    transition={{
+                      duration: 0.4,
+                      delay: 0.8,
+                      ease: "easeIn",
+                    }}
+                  >
+                    Solutions
+                  </motion.span>
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>{NavMenu}</NavigationMenuContent>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>
+                  <motion.span
+                    initial={{ scale: 0.5, opacity: 0 }}
+                    animate={{ scale: 1, opacity: 1 }}
+                    transition={{
+                      duration: 0.4,
+                      delay: 0.9,
+                      ease: "easeIn",
+                    }}
+                  >
+                    Resources
+                  </motion.span>
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>{NavMenu}</NavigationMenuContent>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>
+                  <motion.span
+                    initial={{ scale: 0.5, opacity: 0 }}
+                    animate={{ scale: 1, opacity: 1 }}
+                    transition={{
+                      duration: 0.4,
+                      delay: 1,
+                      ease: "easeIn",
+                    }}
+                  >
+                    Company
+                  </motion.span>
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>{NavMenu}</NavigationMenuContent>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>
+                  <motion.span
+                    initial={{ scale: 0.5, opacity: 0 }}
+                    animate={{ scale: 1, opacity: 1 }}
+                    transition={{
+                      duration: 0.4,
+                      delay: 1.1,
+                      ease: "easeIn",
+                    }}
+                  >
+                    Pricing
+                  </motion.span>
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>{NavMenu}</NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
         </div>
       </div>
       <div className="space-x-4">
         <motion.button
           initial={{ x: "20px", opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          transition={{
-            duration: 0.5,
-            delay: 0.9,
-            ease: "easeIn",
+          animate={{
+            x: 0,
+            opacity: 1,
+            transition: {
+              duration: 0.5,
+              delay: 0.9,
+              ease: "easeIn",
+            },
+          }}
+          whileHover={{
+            scale: 1.1,
+            transition: {
+              type: "spring",
+              stiffness: 100,
+              damping: 10,
+            },
+          }}
+          whileTap={{
+            scale: 0.9,
+            transition: { type: "spring", stiffness: 400, damping: 15 },
           }}
           className="bg-white text-black border py-2 px-3 rounded-lg text-sm border-gray-300"
         >
@@ -116,11 +198,26 @@ export const Navbar = () => {
         </motion.button>
         <motion.button
           initial={{ y: "30px", opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{
-            duration: 0.5,
-            delay: 0.6,
-            ease: "easeOut",
+          animate={{
+            y: 0,
+            opacity: 1,
+            transition: {
+              duration: 0.5,
+              delay: 0.6,
+              ease: "easeOut",
+            },
+          }}
+          whileHover={{
+            scale: 1.1,
+            transition: {
+              type: "spring",
+              stiffness: 100,
+              damping: 10,
+            },
+          }}
+          whileTap={{
+            scale: 0.9,
+            transition: { type: "spring", stiffness: 400, damping: 15 },
           }}
           className="bg-black text-white py-2 px-3 rounded-lg text-sm"
         >
@@ -130,3 +227,28 @@ export const Navbar = () => {
     </motion.div>
   );
 };
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
--- src/components/TopSection.tsx
+++ src/components/TopSection.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 import { Particles } from "./ui/Particles";
 import { motion } from "motion/react";
-import { FaArrowRight } from "react-icons/fa6";
 import { IoArrowForward } from "react-icons/io5";
 
 export const TopSection = () => {
@@ -26,45 +25,99 @@ export const TopSection = () => {
         refresh
       />
 
-      <div className="pt-[5%] flex flex-col gap-10 items-center">
-        <div className="flex gap-3 items-center rounded-lg bg-slate-400/20 py-1 px-4 text-white border text-sm border-slate-300/20 ">
+      <div className="pt-[5%] flex flex-col gap-10 items-center absolute inset-0 ">
+        <motion.div
+          initial={{ y: "-30px", opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{
+            y: { duration: 0.9, ease: "easeIn", delay: 1.4 },
+            opacity: { duration: 1, delay: 1.6, ease: "easeIn" },
+          }}
+          className="flex gap-3 items-center rounded-lg bg-slate-400/20 py-1 px-4 text-white border text-sm border-slate-300/20 "
+        >
           <span>Discover the all new Ganttify 2.0</span>
           <div className="rounded-lg bg-slate-400/20 py-1 px-2">
             <IoArrowForward />
           </div>
-        </div>
+        </motion.div>
 
-        <span className="text-7xl font-bold text-white/70 text-center">
+        <motion.span
+          initial={{ y: "60px", opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{
+            y: { duration: 0.5, ease: "easeIn", delay: 1 },
+            opacity: { duration: 1.1, delay: 1.2, ease: "easeIn" },
+          }}
+          className="text-7xl font-bold text-white/70 text-center"
+        >
           One Tools For Doing It <br /> All Together
-        </span>
+        </motion.span>
 
-        <span className=" font-thin text-base text-white/60 text-center">
+        <motion.span
+          initial={{ y: "40px", opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{
+            y: { duration: 0.8, ease: "easeIn", delay: 1.4 },
+            opacity: { duration: 1, delay: 1.6, ease: "easeInOut" },
+          }}
+          className=" font-thin text-base text-white/60 text-center"
+        >
           Ganttify enables you to achieve clarity and significant results on a
           large scale by <br /> linking tasks and workflows to overarching
           objectives of the company.
-        </span>
-        <div className="flex items-center justify-center gap-7">
+        </motion.span>
+        <div className="flex items-center justify-center gap-5">
           <motion.button
             initial={{ y: "30px", opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{
-              duration: 0.5,
-              delay: 0.6,
-              ease: "easeOut",
+            animate={{
+              y: 0,
+              opacity: 1,
+              transition: {
+                duration: 0.5,
+                delay: 2.0,
+                ease: "easeOut",
+              },
             }}
-            className="bg-white text-black border px-6 py-3  rounded-xl text-sm border-gray-300 font-semibold"
+            whileHover={{
+              scale: 1.1,
+              transition: {
+                type: "spring",
+                stiffness: 100,
+                damping: 10,
+              },
+            }}
+            whileTap={{
+              scale: 0.9,
+              transition: { type: "spring", stiffness: 400, damping: 15 },
+            }}
+            className="bg-white text-black border px-5 py-3  rounded-xl text-sm border-gray-300 font-semibold"
           >
             Get Started
           </motion.button>
           <motion.button
             initial={{ x: "-40px", opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{
-              duration: 0.5,
-              delay: 0.9,
-              ease: "easeIn",
+            animate={{
+              x: 0,
+              opacity: 1,
+              transition: {
+                duration: 0.7,
+                delay: 2.2,
+                ease: "easeIn",
+              },
             }}
-            className="bg-black text-white px-6 py-3  rounded-xl bg-neutral-500/20 border border-slate-300/30 text-sm"
+            whileHover={{
+              scale: 1.1,
+              transition: {
+                type: "spring",
+                stiffness: 100,
+                damping: 10,
+              },
+            }}
+            whileTap={{
+              scale: 0.9,
+              transition: { type: "spring", stiffness: 400, damping: 15 },
+            }}
+            className="bg-black text-white px-5 py-3  rounded-xl bg-neutral-500/20 border border-slate-300/30 text-sm"
           >
             How it Works
           </motion.button>
